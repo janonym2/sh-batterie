e08018224d34a0cb36b84625b4857a6631d4d3fd
janonym2 <jan-hendricks@outlook.de>
2026-01-03 01:17:34 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,39 +5,31 @@ export default function Footer() {
   return (
     <div>
       {/* Footer */}
-      <footer className="bg-[var(--gray)] text-white text-center py-8 text-xs md:text-sm">
-        <p className="mb-4">Platzhalter GmbH · Musterstraße 1, 12345 Musterstadt</p>
-        <ul className="md:flex justify-center gap-6 font-bold text-sm md:text-base">
-          <hr className="md:hidden mx-[25%] my-2" />
+      <footer className="bg-[var(--gray)] text-white py-8 px-[5%] md:px-15 text-sm md:text-sm">
+        <p className="mb-6">Platzhalter GmbH · Musterstraße 1, 12345 Musterstadt</p>
+        <ul className="md:flex gap-6 space-y-6 md:space-y-0 text-sm md:text-base">
           <li>
             <a href="/cookie-pref" className="hover:text-[var(--accent)]">
               Cookie-Einstellungen
             </a>
           </li>
-          <hr className="md:hidden mx-[25%] my-2" />
-          <span className="hidden md:inline">|</span>
           <li>
             <a href="/impressum" className="hover:text-[var(--accent)]">
               Impressum
             </a>
           </li>
-          <hr className="md:hidden mx-[25%] my-2" />
-          <span className="hidden md:inline">|</span>
           <li>
             <a href="/datenschutz" className="hover:text-[var(--accent)]">
               Datenschutzerklärung
             </a>
           </li>
-          <hr className="md:hidden mx-[25%] my-2" />
-          <span className="hidden md:inline">|</span>
           <li>
             <a href="/terms" className="hover:text-[var(--accent)]">
               Mietbedingungen
             </a>
           </li>
-          <hr className="md:hidden mx-[25%] my-2" />
         </ul>
-        <p className="mt-4">&copy; 2025 Platzhalter GmbH. Alle Rechte vorbehalten.</p>
+        <p className="my-6 text-xs">&copy; 2025 Platzhalter GmbH. Alle Rechte vorbehalten.</p>
       </footer>
     </div>
   );
